50ae1b77a01de6bc1c439d1abaf6bda91bfc9314
Svetlana Eliseeva <sve2772@gmail.com>
2024-09-08 21:15:13 +0500

    funcs sort_by_date implemented

--- src/processing.py
+++ src/processing.py
@@ -19,4 +19,13 @@ print(filter_by_state([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03
 
 def sort_by_date(list_state: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
     """Функция сортирует список словарей по дате по убыванию"""
-    pass
+    sorted_list_state = sorted(list_state, key=lambda x: x["date"], reverse=reverse)
+
+    return sorted_list_state
+
+
+print(sort_by_date([{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+                    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+                    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+                    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+                   ))
