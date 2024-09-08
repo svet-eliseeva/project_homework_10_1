29863131faf5bad6cb05e330848f7a093a7fa60c
Svetlana Eliseeva <sve2772@gmail.com>
2024-09-08 20:53:02 +0500

    type annotation and docstring funcs filter_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,8 @@
-def filter_by_state():
+from typing import Any
+
+
+def filter_by_state(list_d: list[dict[str, Any]], state_default: str = 'EXECUTED') -> list[dict[str, Any]]:
+    """ Функция возврата списка словарей по заданному ключу state(состояние) по умолчанию 'EXECUTED'(выполнено)"""
     pass
 
 
