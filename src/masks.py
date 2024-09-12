f4efcfcfade35e92e15d61c13e9c3e79afd3d98a
Svetlana Eliseeva <sve2772@gmail.com>
2024-09-12 14:10:50 +0500

    add funcs in masks.py

---
+++ src/masks.py
@@ -0,0 +1,20 @@
+from typing import Union, Any, Optional
+
+
+def get_mask_card_number(card_number: int) -> str:
+    """Функция маскировки номера банковской карты"""
+    card = str(card_number)
+
+    return f'{card[:4]} {card[5:7]}** **** {card[-4:]}'
+
+
+print(get_mask_card_number(7000792289606361))
+
+
+def get_mask_account(account: int) -> str:
+    """Функция маскировки номера банковского счета"""
+    mask_account = str(account)
+    return f'**{mask_account[-4:]}'
+
+
+print(get_mask_account(73654108430135874305))
